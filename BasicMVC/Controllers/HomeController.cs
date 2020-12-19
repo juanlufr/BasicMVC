5cf75d5a790e9d9c695c9d52d86b5afa2adaa79d
juanlufr <juanlufr@users.noreply.github.com>
2020-12-19 19:27:03 +0100

    Initial commit

---
+++ BasicMVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using BasicMVC.Models;
+
+namespace BasicMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var coches = new List<Coche>
+            {
+                new Coche {Marca="BMW",Modelo="X6",Color="Gris"},
+                new Coche {Marca="Mercedes",Modelo="A200",Color="Blanco"},
+                new Coche {Marca="Porsche",Modelo="Cayanne",Color="Azul"},
+                new Coche {Marca="Ford",Modelo="Mustang",Color="Rojo"}
+            };
+            
+            return View(coches);
+        }
+    }
+}
---
+++ BasicMVC/Models/Coche.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicMVC.Models
+{
+    public class Coche
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+    }
+}
